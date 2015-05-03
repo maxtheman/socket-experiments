66b991435e7fe5db7dfe96560dc92bd7ed975445
Max Caldwell <maxcaldwell1@gmail.com>
2015-05-03 15:50:34 -0700

    write script options

--- socket_experiments.py
+++ socket_experiments.py
@@ -4,3 +4,15 @@ import optparse
 host_name = "www.google.com"
 
 print socket.gethostbyname(host_name)
+
+usage = 'usage: %prog -H <target host> -p <target port>'
+
+parser = optparse.OptionParser(usage=usage)
+parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
+parser.add_option('-p', dest='tgtPort', type='int', help='specify target port')
+(options,args) = parser.parse_args()
+target_host = options.tgtHost
+target_port = options.tgtPort
+if (target_host == None) or (target_port == None):
+	print parser.usage
+	exit(0)
